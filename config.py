7970f1101cda1ff0302e1f55ee6bb0ae8590d3d3
fahimfahimgaminggaming-sys <fahimfahimgaminggaming@gmail.com>
2026-03-10 22:17:33 +0600

    config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+BOT_TOKEN = "8719865837:AAE3dmy2T3hmjEWLaOjDyRi5kU9zb9DY5t8"
+
+ADMIN_ID = 7015857680
+
+MONGO_URI = "mongodb+srv://db_Botuser:db_botpass123@cluster0.hrcnqqs.mongodb.net/?appName=Cluster0"
+
+NOWPAY_API_KEY = "9EPK9XN-B404VJE-NSVCSS3-9WZN10J"
