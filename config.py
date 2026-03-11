6eed053c039f3cb00571601abb8bdc059b515bb9
fahimfahimgaminggaming-sys <fahimfahimgaminggaming@gmail.com>
2026-03-12 05:06:31 +0600

    config.py

--- config.py
+++ config.py
@@ -1,19 +1,8 @@
-# ================= BOT =================
 
 BOT_TOKEN = "8719865837:AAE3dmy2T3hmjEWLaOjDyRi5kU9zb9DY5t8"
-
 ADMIN_ID = 7015857680
 
-
-# ================= DATABASE =================
-
 MONGO_URI = "mongodb+srv://db_Botuser:db_botpass123@cluster0.hrcnqqs.mongodb.net/?retryWrites=true&w=majority&tls=true"
-
-
-# ================= PAYMENT =================
-
-# Telegram Stars payment (empty রাখবে)
-STARS_PROVIDER_TOKEN = ""
 
 # CryptoBot API
 CRYPTOBOT_TOKEN = "547846:AAUY6PdNQEvvS5xCJj4Ow8fApdQ5OAWvPyx"
@@ -28,17 +17,3 @@ XROCKET_API_KEY =  "23cf85f9e9e760a095ba3581c"
 # ================= SETTINGS =================
 
 PC_COIN_PRICE = 0.013
-
-
-# ================= CHANNELS =================
-
-FORCE_CHANNELS = [
-    "@Task_Updates_Group",
-    "@Crypto_Tech_Zone",
-    "@AIRDROP_OFFICIALBD20"
-]
-
-
-# ================= TASK GROUP =================
-
-TASK_GROUP = "@Task_Updates_Group"
